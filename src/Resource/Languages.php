783756b6ad66683f87a4863f9d84548f92d01707
Bart Tyrant <b.tyranowski@gmail.com>
2015-04-13 16:07:01 +0200

    Languages support (not ready yet)

---
+++ src/Resource/Languages.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Genderize\Resource;
+
+/**
+ * Description of Languages
+ *
+ * @author barttyrant
+ */
+class Languages {
+
+    public static function get() {
+        return [
+                // add language support here
+        ];
+    }
+
+}
